873858232728ecdd451cb83e95089b0669072aef
bashgirlie <94557891+bashgirlie@users.noreply.github.com>
2026-07-15 16:54:48 -0500

    Create send_keys.py

---
+++ robot/send_keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+import sockets
+import pynput
+
+"""
+Script to take key inputs from a laptop pynput, encode and send them as useful data to a robot via UDP with sockets while the robot is in AP mode (which the laptop connects to).
+"""
